8c7f8cac8f63dcdb342b1674651ea9837e9e8161
Nurçin <nrcinozer@gmail.com>
2022-09-29 22:35:31 +0300

    refactor button classname

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 export type ButtonVariant =
   | 'primary'
   | 'secondary';
@@ -15,16 +17,15 @@ export const Button: React.FC<ButtonProps> = ({
   variant = 'primary',
 }) => {
 
-  const baseStyle = 'inline-flex items-center border-0 focus:outline-none rounded mt-4 md:mt-0';
-  const primaryStyle = 'bg-indigo-500 hover:bg-indigo-600';
-  const secondaryStyle = ' bg-gray-800 hover:bg-gray-700';
-  const largeStyle = 'text-lg';
-  const smallStyle = 'text-base py-1 px-3';
+  const className = useMemo(() => {
+    const base = 'inline-flex items-center border-0 focus:outline-none rounded mt-4 md:mt-0';
+    const sizeClass = size === 'large' ? 'text-lg' : 'text-sm py-2 px-3';
+    const variantClass = variant === 'primary' ? 'bg-indigo-500 hover:bg-indigo-600' : 'bg-gray-800 hover:bg-gray-700';
+    return `${base} ${sizeClass} ${variantClass}`;
+  }, [size, variant]);
 
   return (
-    <button className={
-      `${baseStyle} ${variant === 'primary' ? primaryStyle : secondaryStyle} ${size === 'large' ? largeStyle : smallStyle}`
-    }>
+    <button className={className}>
       {children}
     </button>
   )
